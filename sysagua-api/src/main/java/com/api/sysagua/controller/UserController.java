c00301c996a349d64de3150e6d8d7fdae77b4120
CristianoMends <mendescristiano012@gmail.com>
2024-12-10 22:18:51 -0300

    refactor: adicionado endpoints de criar conta de usuario e fazer login

--- sysagua-api/src/main/java/com/api/sysagua/controller/UserController.java
+++ sysagua-api/src/main/java/com/api/sysagua/controller/UserController.java
@@ -1,21 +1,44 @@
 package com.api.sysagua.controller;
 
 
+import com.api.sysagua.dto.CreateUserDto;
 import com.api.sysagua.dto.LoginDto;
 import com.api.sysagua.dto.Token;
+import com.api.sysagua.model.User;
+import com.api.sysagua.service.AuthorizationService;
+import com.api.sysagua.service.TokenService;
+import com.api.sysagua.service.UserService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Objects;
 
 @RestController
 @RequestMapping("/users")
 @Tag(name = "User Controller", description = "Contém endpoints para gerenciamento de usuários")
 public class UserController {
 
+    @Autowired
+    private AuthorizationService authorizationService;
+    @Autowired
+    private UserService userService;
+    @Autowired
+    private TokenService tokenService;
+    @Autowired
+    AuthenticationManager authenticationManager;
+
+    @CrossOrigin
+    @PostMapping("create")
+    public ResponseEntity<String> createUser(@RequestBody @Valid CreateUserDto userDto) {
+        userService.createUser(userDto);
+        return ResponseEntity.status(HttpStatus.CREATED).body("");
+    }
 
     @PostMapping("login")
     @CrossOrigin
@@ -23,11 +46,11 @@ public class UserController {
     public ResponseEntity<Token> login(
             @RequestBody LoginDto loginDto
     ) {
-        if (!loginDto.email().equals("usuario@gmail.com") || !loginDto.password().equals("qwe123")){
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
-        }
-        return ResponseEntity.status(HttpStatus.OK).body(new Token("qwe121qw2e1q2w1e2q1sd"));
-    }
+        var usernamePassword = new UsernamePasswordAuthenticationToken(loginDto.email(), loginDto.password());
+        var auth = this.authenticationManager.authenticate(usernamePassword);
 
+        var token = this.tokenService.generateToken( (User) auth.getPrincipal() );
+        return ResponseEntity.status(HttpStatus.OK).body(new Token(token));
+    }
 
 }
