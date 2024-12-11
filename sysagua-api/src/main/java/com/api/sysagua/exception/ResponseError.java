b27ed2ece16176777c6c9e1358f5637175e44d60
CristianoMends <mendescristiano012@gmail.com>
2024-12-10 23:05:36 -0300

    feat: add classe para resposta tipo erro

---
+++ sysagua-api/src/main/java/com/api/sysagua/exception/ResponseError.java
@@ -0,0 +1,30 @@
+package com.api.sysagua.exception;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class ResponseError {
+    private LocalDateTime timestamp = LocalDateTime.now();
+    private String status = "error";
+    private int statusCode = 400;
+    private String error;
+
+    public ResponseError(String error, int statusCode) {
+        this.error = error;
+        this.statusCode = statusCode;
+        this.timestamp = LocalDateTime.now();
+    }
+
+    public ResponseError(String error, int statusCode, String status) {
+        this.error = error;
+        this.statusCode = statusCode;
+        this.status = status;
+        this.timestamp = LocalDateTime.now();
+    }
+}
