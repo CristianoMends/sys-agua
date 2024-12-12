394348ab4da69e6077ae879c9b2d1db79a169463
CristianoMends <mendescristiano012@gmail.com>
2024-12-12 18:31:55 -0300

    refactor: usando retornos com Optional

--- sysagua-api/src/main/java/com/api/sysagua/repository/UserRepository.java
+++ sysagua-api/src/main/java/com/api/sysagua/repository/UserRepository.java
@@ -4,10 +4,11 @@ import com.api.sysagua.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
-    UserDetails findByEmail(String email);
+    Optional<UserDetails> findByEmail(String email);
 
-    User findByPhone(String phone);
+    Optional<User> findByPhone(String phone);
 }
