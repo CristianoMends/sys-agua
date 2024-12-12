0950e2ee4404556b2e05b9cfd0dc6a5daf855f34
CristianoMends <mendescristiano012@gmail.com>
2024-12-12 18:19:54 -0300

    feat: adicionado schema para Swagguer docs

--- sysagua-api/src/main/java/com/api/sysagua/dto/Token.java
+++ sysagua-api/src/main/java/com/api/sysagua/dto/Token.java
@@ -1,3 +1,12 @@
 package com.api.sysagua.dto;
 
-public record Token(String token) {}
+import io.swagger.v3.oas.annotations.media.Schema;
+
+/**
+ * DTO que representa um token de autenticação JWT.
+ */
+@Schema(description = "DTO que contém o token de autenticação JWT gerado para o usuário")
+public record Token(
+
+        @Schema(description = "O token JWT gerado após o login", example = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoxMjM0NTY3ODkwLCJpYXQiOjE2MTc5Mzg3NzksImV4cCI6MTYxODAxMzc3OX0.VyA5FtkErLzLqkzqMhcbqNnJEXhGcjybY0aG_gTwAFA")
+        String token) {}
