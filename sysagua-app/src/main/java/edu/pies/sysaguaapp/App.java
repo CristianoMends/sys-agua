21d14762fb48d1f0fd71e2eb4c38b1d03c90098b
Rafael Leite <costtaraphael@gmail.com>
2024-12-13 14:00:30 -0300

    feat: adicionei a tela de HomeView no app

--- sysagua-app/src/main/java/edu/pies/sysaguaapp/App.java
+++ sysagua-app/src/main/java/edu/pies/sysaguaapp/App.java
@@ -3,21 +3,24 @@ package edu.pies.sysaguaapp;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
-import java.io.IOException;
+import java.util.Objects;
 
-public class App extends Application {
-    @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
-    }
+public class App extends Application {@Override
+public void start(Stage primaryStage) throws Exception {
+    FXMLLoader loader = new FXMLLoader(getClass().getResource("/views/HomeView.fxml"));
+    BorderPane root = loader.load();
+    Scene scene = new Scene(root, 800, 600);
+    scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("/styles.css")).toExternalForm());
+
+    primaryStage.setTitle("SysAgua - Controle de Pedidos");
+    primaryStage.setScene(scene);
+    primaryStage.show();
+}
 
     public static void main(String[] args) {
-        launch();
+        launch(args);
     }
 }
