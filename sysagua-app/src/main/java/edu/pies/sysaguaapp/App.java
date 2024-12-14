95f3de4b3fcccc942680db2a6c75aecaf9deaa28
Rafael Leite <costtaraphael@gmail.com>
2024-12-14 13:56:19 -0300

    feat: mudei de borderPane para Stackpane

--- sysagua-app/src/main/java/edu/pies/sysaguaapp/App.java
+++ sysagua-app/src/main/java/edu/pies/sysaguaapp/App.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 import java.util.Objects;
@@ -11,7 +12,7 @@ import java.util.Objects;
 public class App extends Application {@Override
 public void start(Stage primaryStage) throws Exception {
     FXMLLoader loader = new FXMLLoader(getClass().getResource("/views/HomeView.fxml"));
-    BorderPane root = loader.load();
+    StackPane root = loader.load();
     Scene scene = new Scene(root, 800, 600);
     scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("/styles.css")).toExternalForm());
 
