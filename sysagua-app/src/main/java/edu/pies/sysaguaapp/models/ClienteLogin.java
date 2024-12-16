90e1d8ae094778e2371394094957dae3e154ddeb
josiasdev <francisco.batista67@alu.ufc.br>
2024-12-15 23:25:49 -0300

    feat: modelo cliente para login com email e senha

--- sysagua-app/src/main/java/edu/pies/sysaguaapp/models/ClienteLogin.java
+++ sysagua-app/src/main/java/edu/pies/sysaguaapp/models/ClienteLogin.java
@@ -4,38 +4,21 @@ import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 
 public class ClienteLogin {
-    private final StringProperty nome;
-    private final StringProperty cnpj;
-    private final StringProperty endereco;
-    private final StringProperty cidade;
     private final StringProperty email;
+    private final StringProperty senha;
 
-    public ClienteLogin(String nome, String cnpj, String endereco, String cidade, String email) {
-        this.nome = new SimpleStringProperty(nome) ;
-        this.cnpj = new SimpleStringProperty(cnpj) ;
-        this.endereco = new SimpleStringProperty(endereco) ;
-        this.cidade = new SimpleStringProperty(cidade) ;
-        this.email = new SimpleStringProperty(email) ;
+
+    public ClienteLogin(String email, String senha) {
+        this.email = new SimpleStringProperty(email);
+        this.senha = new SimpleStringProperty(senha);
     }
 
     // Getters e Setters
-    public StringProperty getNome() {
-        return nome;
-    }
-
-    public StringProperty getCnpj() {
-        return cnpj;
-    }
-
-    public StringProperty getEndereco() {
-        return endereco;
-    }
-
-    public StringProperty getCidade() {
-        return cidade;
-    }
-
     public StringProperty getEmail() {
         return email;
+    }
+
+    public StringProperty getSenha() {
+        return senha;
     }
 }
