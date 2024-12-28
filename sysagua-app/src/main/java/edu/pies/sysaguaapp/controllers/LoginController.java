39e9d452aa309d73beb32aa8ea8f212663f22539
Rafael Leite <costtaraphael@gmail.com>
2024-12-28 18:07:23 -0300

    adjustment: ajustes de imagem de login e dados de login

--- sysagua-app/src/main/java/edu/pies/sysaguaapp/controllers/LoginController.java
+++ sysagua-app/src/main/java/edu/pies/sysaguaapp/controllers/LoginController.java
@@ -3,14 +3,11 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 import java.io.IOException;
-import java.util.Objects;
 
 public class LoginController {
     @FXML
@@ -21,8 +18,8 @@ public class LoginController {
     @FXML
     private void initialize() {
         // Dados hardcoded para simular um login
-        String email = "josiasmartins@gmail.com";
-        String senha = "123";
+        String email = "cliente@cliente.com";
+        String senha = "12345678";
 
         // Preenche os TextFields com os dados
         emailField.setText(email);
@@ -34,20 +31,20 @@ public class LoginController {
     @FXML
     private void handleLoginButtonAction(ActionEvent event) {
         try {
-            // Carrega o arquivo HomeView.fxml
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("/views/HomeView.fxml"));
+            // Carrega o arquivo Home.fxml
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/views/Home.fxml"));
             StackPane homeView = loader.load();
             // Obtém a janela atual (Stage) e troca a cena
             Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
             Scene scene = new Scene(homeView, 800, 600);
 
             stage.setScene(scene);
-            stage.setTitle("SysAgua - Home");
+            stage.setTitle("SysAgua - Inicio");
             stage.show();
 
         } catch (IOException e) {
             e.printStackTrace();
-            System.out.println("Erro ao carregar a tela HomeView.fxml");
+            System.out.println("Erro ao carregar a tela Home.fxml");
         }
     }
 }
