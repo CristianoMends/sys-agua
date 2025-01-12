2a38b5669d53dd5523c0cc0df13ffce43cf0bcad
CristianoMends <mendescristiano012@gmail.com>
2025-01-12 10:08:39 -0300

    refactor: add endpoints deletar e atualizar

--- sysagua-api/src/main/java/com/api/sysagua/controller/CustomerController.java
+++ sysagua-api/src/main/java/com/api/sysagua/controller/CustomerController.java
@@ -2,6 +2,7 @@ package com.api.sysagua.controller;
 
 import com.api.sysagua.dto.customer.CreateCustomerDto;
 import com.api.sysagua.dto.customer.SearchCustomerDto;
+import com.api.sysagua.dto.customer.UpdateCustomerDto;
 import com.api.sysagua.exception.ResponseError;
 import com.api.sysagua.model.Customer;
 import com.api.sysagua.service.CustomerService;
@@ -11,14 +12,11 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
-import jakarta.validation.constraints.Pattern;
-import jakarta.validation.constraints.Size;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.time.LocalDate;
 import java.util.List;
 
 @RestController
@@ -102,13 +100,88 @@ public class CustomerController {
             @RequestParam(required = false) String neighborhood,
             @RequestParam(required = false) String city,
             @RequestParam(required = false) String state,
-            @RequestParam(required = false) Boolean active
+            @RequestParam(required = false) Boolean active,
+            @RequestParam(required = false) String cnpj
     ) {
         var search = new SearchCustomerDto(
-                id,name,street,neighborhood,city,state,phone, active
+                id,name,street,neighborhood,city,state,phone, active, cnpj
         );
 
         List<Customer> customers = service.findByFilters(search);
         return ResponseEntity.ok(customers);
+    }
+
+    @Operation(
+            summary = "Deletar (inativar) um cliente",
+            description = "Marca o cliente como inativo com base no ID fornecido.",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "204",
+                            description = "Cliente deletado (inativado) com sucesso.",
+                            content = @Content()
+                    ),
+                    @ApiResponse(
+                            responseCode = "404",
+                            description = "Cliente não encontrado.",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = ResponseError.class)
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "403",
+                            description = "Não autorizado.",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = ResponseError.class)
+                            )
+                    )
+            }
+    )
+    @DeleteMapping("{id}")
+    public ResponseEntity<Void> deleteCustomer(@PathVariable Long id){
+        this.service.deleteCustomer(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @Operation(
+            summary = "Atualizar informações de um cliente",
+            description = "Atualiza as informações de um cliente com base no ID fornecido. Os dados a serem atualizados são fornecidos no corpo da requisição.",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "204",
+                            description = "Cliente atualizado com sucesso.",
+                            content = @Content()
+                    ),
+                    @ApiResponse(
+                            responseCode = "400",
+                            description = "Dados inválidos.",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = ResponseError.class)
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "404",
+                            description = "Cliente não encontrado.",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = ResponseError.class)
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "403",
+                            description = "Não autorizado.",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = ResponseError.class)
+                            )
+                    )
+            }
+    )
+    @PutMapping("{id}")
+    public ResponseEntity<Void> updateCustomer(@PathVariable Long id, @RequestBody @Valid UpdateCustomerDto dto){
+        this.service.updateCustomer(id,dto);
+        return ResponseEntity.noContent().build();
     }
 }
