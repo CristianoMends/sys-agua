577058b2a58cc27d57f9a5e66081942d52867d3e
Rafael Leite <costtaraphael@gmail.com>
2025-01-14 21:42:52 -0300

    Feat: criei o modelo de entregador com base na documentação

---
+++ sysagua-app/src/main/java/edu/pies/sysaguaapp/models/Entregador.java
@@ -0,0 +1,12 @@
+package edu.pies.sysaguaapp.models;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Entregador {
+    private String nome;
+    private String telefone;
+}
