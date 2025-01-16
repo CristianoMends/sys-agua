34cd21752e715aa9d675576698274ac20ba9aad0
CristianoMends <mendescristiano012@gmail.com>
2025-01-16 19:55:20 -0300

    feat: add migrations para h2

---
+++ sysagua-api/src/main/resources/db/migration/h2/V5__insert_into_products.sql
@@ -0,0 +1,12 @@
+INSERT INTO Products (name, unit, brand, category, created_at, updated_At, active)
+VALUES
+('Garrafa de Água 500ml', 'ml', 'Cristalina', 'Água Mineral', '2025-01-12', '2025-01-12', true),
+('Garrafa de Água 1L', 'L', 'Cristalina', 'Água Mineral', '2025-01-12', '2025-01-12', true),
+('Garrafa de Água 1.5L', 'L', 'Cristalina', 'Água Mineral', '2025-01-12', '2025-01-12', true),
+('Garrafa de Água 5L', 'L', 'Cristalina', 'Água Mineral', '2025-01-12', '2025-01-12', true),
+('Garrafa de Água 20L', 'L', 'Cristalina', 'Água Mineral', '2025-01-12', '2025-01-12', false),
+('Água com Gás 500ml', 'ml', 'Fonte Pura', 'Água com Gás', '2025-01-12', '2025-01-12', false),
+('Água com Gás 1L', 'L', 'Fonte Pura', 'Água com Gás', '2025-01-12', '2025-01-12', true),
+('Água Saborizada 500ml - Limão', 'ml', 'HidraFresh', 'Água Saborizada', '2025-01-12', '2025-01-12', true),
+('Água Saborizada 500ml - Maçã', 'ml', 'HidraFresh', 'Água Saborizada', '2025-01-12', '2025-01-12', false),
+('Água Saborizada 1L - Frutas Vermelhas', 'L', 'HidraFresh', 'Água Saborizada', '2025-01-12', '2025-01-12', true);
