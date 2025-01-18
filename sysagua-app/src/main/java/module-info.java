a9ecf4aa9f460a950f2abee4969028cf764f1778
josiasdev <francisco.batista67@alu.ufc.br>
2025-01-18 15:58:51 -0300

    refactor: tela de login e imports removidos

--- sysagua-app/src/main/java/module-info.java
+++ sysagua-app/src/main/java/module-info.java
@@ -7,7 +7,6 @@ module edu.pies.sysaguaapp {
     requires org.kordamp.bootstrapfx.core;
     requires java.net.http;
     requires java.xml.crypto;
-    requires com.fasterxml.jackson.datatype.jsr310;
     requires com.fasterxml.jackson.databind;
 
 
