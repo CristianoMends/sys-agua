a1f86929e09752e123aea5eb9f30053baaf8bd05
josiasdev <francisco.batista67@alu.ufc.br>
2025-01-21 08:54:31 -0300

    feat: buscar cliente por id, editar e excluir clientes

--- sysagua-app/src/main/java/edu/pies/sysaguaapp/services/ClientesService.java
+++ sysagua-app/src/main/java/edu/pies/sysaguaapp/services/ClientesService.java
@@ -38,13 +38,9 @@ public class ClientesService {
     }
 
     public static Clientes criarCliente(Clientes clientes, String token) throws Exception {
-        // Converter o objeto Produto para JSON
-
         String clienteJson = objectMapper.writeValueAsString(clientes);
         System.out.println("Sending client data: " + clienteJson);
 
-
-        // Criar a requisição POST
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create(BASE_URL))
                 .POST(HttpRequest.BodyPublishers.ofString(clienteJson))
@@ -65,5 +61,74 @@ public class ClientesService {
             throw new Exception("Erro ao criar cliente: " + response.body());
         }
         return clientes;
+    }
+    public static Clientes editarCliente(Long id, Clientes clienteAtualizado) throws Exception {
+        String token = TokenManager.getInstance().getToken();
+        if (token == null || token.isEmpty()) {
+            throw new Exception("Token não encontrado. Por favor, faça login.");
+        }
+
+        String clienteJson = objectMapper.writeValueAsString(clienteAtualizado);
+
+        // Criar a requisição PUT
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(BASE_URL + "/" + id))
+                .PUT(HttpRequest.BodyPublishers.ofString(clienteJson))
+                .header("Content-Type", "application/json")
+                .header("Authorization", "Bearer " + token)
+                .build();
+
+        // Enviar a requisição
+        HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+
+        // Verificar a resposta
+        if (response.statusCode() == 204) {
+            return clienteAtualizado;
+        } else if (response.statusCode() == 404) {
+            throw new Exception("Cliente não encontrado: " + response.body());
+        }
+        else {
+            throw new Exception("Erro ao editar cliente: " + response.body());
+        }
+    }
+
+    public Clientes buscarClientePorId(Long id, String token) throws Exception {
+        String url = BASE_URL + "/" + id;
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(url))
+                .GET()
+                .header("Content-Type", "application/json")
+                .header("Authorization", "Bearer " + token)
+                .build();
+
+        HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+
+        if (response.statusCode() == 200) {
+            return objectMapper.readValue(response.body(), Clientes.class);
+        } else {
+            throw new Exception("Erro ao buscar cliente: " + response.body());
+        }
+    }
+
+    public static void excluirCliente(Long id) throws Exception {
+        String token = TokenManager.getInstance().getToken();
+        if (token == null || token.isEmpty()) {
+            throw new Exception("Token não encontrado. Por favor, faça login.");
+        }
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(BASE_URL + "/" + id))
+                .DELETE()
+                .header("Authorization", "Bearer " + token)
+                .build();
+
+        // Enviar a requisição
+        HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+
+        // Verificar a resposta
+        if (response.statusCode() == 200 || response.statusCode() == 204) {
+            System.out.println("Cliente excluído com sucesso.");
+        } else {
+            throw new Exception("Erro ao excluir cliente: " + response.body());
+        }
     }
 }
