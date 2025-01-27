6ef596c29bab1d34613eefc0690518a510c6dbe1
Rafael Leite <137407431+Rafaelleit3@users.noreply.github.com>
2025-01-27 09:47:11 -0300

    Revert "api/feature/deliverer"

--- sysagua-api/src/main/resources/db/migration/h2/V20__insert_into_deliverer.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO deliverer (name, phone, created_at, active) VALUES
-('William', '5585982123209', '2025-01-20', FALSE),
-('Jasmine', '5585940022892', '2025-01-21', FALSE),
-('Elimar', '5588985479236', '2025-01-22', TRUE),
-('Francisco', '5585910257863', '2025-01-23', TRUE),
-('Paulo', '5588998760725', '2025-01-24', FALSE),
-('Jonathan', '5588944276380', '2025-01-25', TRUE);
-
