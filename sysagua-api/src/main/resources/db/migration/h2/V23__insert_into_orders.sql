26518b001ac2e788254c712c9ce0f0a3243c72ea
CristianoMends <mendescristiano012@gmail.com>
2025-02-03 20:36:51 -0300

    refactor: 1 - mudança no metodo de pagamento

--- sysagua-api/src/main/resources/db/migration/h2/V23__insert_into_orders.sql
+++ sysagua-api/src/main/resources/db/migration/h2/V23__insert_into_orders.sql
@@ -1,2 +1,2 @@
 INSERT INTO orders (customer_id, delivery_person_id, status, received_amount, total_amount, payment_method, created_at, finished_at)
-VALUES (1, 1, 'PENDING', 50.00, 50.00, 'CASH', NOW(), NULL);
+VALUES (1, 1, 'PENDING', 50.00, 50.00, 'MONEY', NOW(), NULL);
