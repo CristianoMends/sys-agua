ef72bb15ac3e859d3d6b2ee5911c436a568417a0
CristianoMends <mendescristiano012@gmail.com>
2025-02-04 11:19:57 -0300

    feat: criado metodos para ajustar o estoque conforme a transação e quando finalizar

--- sysagua-api/src/main/java/com/api/sysagua/service/impl/TransactionServiceImpl.java
+++ sysagua-api/src/main/java/com/api/sysagua/service/impl/TransactionServiceImpl.java
@@ -1,10 +1,13 @@
 package com.api.sysagua.service.impl;
 
+import com.api.sysagua.dto.transaction.ViewTransactionDto;
+import com.api.sysagua.enumeration.OrderStatus;
 import com.api.sysagua.enumeration.PaymentMethod;
-import com.api.sysagua.enumeration.StatusTransaction;
+import com.api.sysagua.enumeration.TransactionStatus;
 import com.api.sysagua.enumeration.TransactionType;
 import com.api.sysagua.exception.BusinessException;
-import com.api.sysagua.model.Transaction;
+import com.api.sysagua.model.*;
+import com.api.sysagua.repository.StockRepository;
 import com.api.sysagua.repository.TransactionRepository;
 import com.api.sysagua.service.TransactionService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,34 +22,66 @@ import java.util.List;
 public class TransactionServiceImpl implements TransactionService {
     @Autowired
     private TransactionRepository transactionRepository;
+    @Autowired
+    private StockRepository stockRepository;
 
     @Override
-    public void save(BigDecimal amount, TransactionType type, PaymentMethod paymentMethod, String description) {
-        Transaction transaction = new Transaction();
-        transaction.setAmount(amount);
-        transaction.setStatus(StatusTransaction.PENDING);
-        transaction.setDescription(description);
-        transaction.setPaymentMethod(paymentMethod);
-        transaction.setType(type);
+    public void save(Transaction transaction) {
         this.transactionRepository.save(transaction);
     }
 
     @Override
     public void cancel(Long id) {
-        var t = this.transactionRepository.findById(id).orElseThrow(() -> new BusinessException("Transaction not found", HttpStatus.NOT_FOUND));
-        t.setCanceledAt(LocalDateTime.now());
-        this.transactionRepository.save(t);
+        var transaction = this.transactionRepository.findById(id).orElseThrow(() -> new BusinessException("Transaction not found", HttpStatus.NOT_FOUND));
+
+        transaction.setCanceledAt(LocalDateTime.now());
+        transaction.setStatus(TransactionStatus.CANCELED);
+        this.transactionRepository.save(transaction);
     }
 
     @Override
     public void finish(Long id) {
-        var t = this.transactionRepository.findById(id).orElseThrow(() -> new BusinessException("Transaction not found", HttpStatus.NOT_FOUND));
-        t.setFinishedAt(LocalDateTime.now());
-        this.transactionRepository.save(t);
+        var transaction = this.transactionRepository.findById(id).orElseThrow(() -> new BusinessException("Transaction not found", HttpStatus.NOT_FOUND));
+        if (!transaction.getStatus().equals(TransactionStatus.PENDING)) throw new BusinessException("Transaction is not PENDING");
+
+        if (transaction.getOrder() != null) {
+            transaction.getOrder().setStatus(OrderStatus.FINISHED);
+            processOrderProducts(transaction.getOrder());
+        }
+        if (transaction.getPurchase() != null) processPurchaseProducts(transaction.getPurchase());
+
+        transaction.setFinishedAt(LocalDateTime.now());
+        transaction.setStatus(TransactionStatus.PAID);
+        this.transactionRepository.save(transaction);
     }
 
+    private void addWithdrawsProductFromStock(int quantity, Product product) {
+        var stock = this.stockRepository.findProduct(product.getId()).orElseThrow(
+                () -> new BusinessException("Stock by product not found", HttpStatus.NOT_FOUND)
+        );
+        stock.setTotalWithdrawals(stock.getTotalWithdrawals() + quantity);
+        this.stockRepository.save(stock);
+    }
+    private void addEntriesProductFromStock(int quantity, Product product) {
+        var stock = this.stockRepository.findProduct(product.getId()).orElseThrow(
+                () -> new BusinessException("Stock by product not found", HttpStatus.NOT_FOUND)
+        );
+
+        stock.setTotalEntries(stock.getTotalEntries() + quantity);
+        this.stockRepository.save(stock);
+    }
+    private void processPurchaseProducts(Purchase purchase) {
+        purchase.getProductPurchases()
+                .forEach(p -> addEntriesProductFromStock(p.getQuantity(), p.getProduct()));
+    }
+    private void processOrderProducts(Order order) {
+        order.getProductOrders()
+                .forEach(p -> addWithdrawsProductFromStock(p.getQuantity(), p.getProduct()));
+    }
+
+
     @Override
-    public List<Transaction> list(Long id, StatusTransaction status, BigDecimal amountStart, BigDecimal amountEnd, TransactionType type, PaymentMethod paymentMethod, String description, LocalDateTime createdAtStart, LocalDateTime createdAtEnd, LocalDateTime finishedAtStart, LocalDateTime finishedAtEnd, LocalDateTime canceledAtStart, LocalDateTime canceledAtEnd) {
+    public List<ViewTransactionDto> list(Long id, TransactionStatus status, BigDecimal amountStart, BigDecimal amountEnd, TransactionType type, PaymentMethod paymentMethod, String description, LocalDateTime createdAtStart, LocalDateTime createdAtEnd, LocalDateTime finishedAtStart, LocalDateTime finishedAtEnd, LocalDateTime canceledAtStart, LocalDateTime canceledAtEnd) {
         return this.transactionRepository.list(
                 id,
                 status,
@@ -61,7 +96,7 @@ public class TransactionServiceImpl implements TransactionService {
                 finishedAtEnd,
                 canceledAtStart,
                 canceledAtEnd
-        );
+        ).stream().map(Transaction::toView).toList();
     }
 
 
