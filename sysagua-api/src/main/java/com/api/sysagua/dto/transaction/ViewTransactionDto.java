e7423125c2bf625fe092afba0acfd5a3c5f30f7a
CristianoMends <mendescristiano012@gmail.com>
2025-02-07 12:28:12 -0300

    refactor: add status de transação

--- sysagua-api/src/main/java/com/api/sysagua/dto/transaction/ViewTransactionDto.java
+++ sysagua-api/src/main/java/com/api/sysagua/dto/transaction/ViewTransactionDto.java
@@ -20,7 +20,7 @@ public class ViewTransactionDto {
     @Schema(description = "Identificador único da transação", example = "1")
     private Long id;
 
-    @Schema(description = "Status da transação", example = "COMPLETED")
+    @Schema(description = "Status da transação", example = "PENDING")
     private TransactionStatus status;
 
     @Schema(description = "Data e hora em que a transação foi criada", example = "2024-02-04T12:30:45")
@@ -35,13 +35,13 @@ public class ViewTransactionDto {
     @Schema(description = "Valor da transação", example = "250.75")
     private BigDecimal amount;
 
-    @Schema(description = "Tipo da transação", example = "PAYMENT")
+    @Schema(description = "Tipo da transação", example = "INCOME")
     private TransactionType type;
 
     @Schema(description = "Método de pagamento utilizado", example = "CREDIT_CARD")
     private PaymentMethod paymentMethod;
 
-    @Schema(description = "Descrição da transação", example = "Pagamento de compra")
+    @Schema(description = "Descrição da transação", example = "Pagamento de Pedido")
     private String description;
 
     @Schema(description = "ID do pedido associado à transação, se aplicável", example = "123")
