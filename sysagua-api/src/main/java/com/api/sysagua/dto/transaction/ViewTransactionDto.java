7ed9e138205db0b55504bfbe2fdb1cc873984410
CristianoMends <mendescristiano012@gmail.com>
2025-02-08 22:14:09 -0300

    refactor: adicionado visão completa de compra ou pedido em transações

--- sysagua-api/src/main/java/com/api/sysagua/dto/transaction/ViewTransactionDto.java
+++ sysagua-api/src/main/java/com/api/sysagua/dto/transaction/ViewTransactionDto.java
@@ -1,6 +1,7 @@
 package com.api.sysagua.dto.transaction;
 
-import com.api.sysagua.enumeration.PaymentMethod;
+import com.api.sysagua.dto.order.ViewOrderDto;
+import com.api.sysagua.dto.purchase.ViewPurchaseDto;
 import com.api.sysagua.enumeration.TransactionStatus;
 import com.api.sysagua.enumeration.TransactionType;
 import io.swagger.v3.oas.annotations.media.Schema;
@@ -26,27 +27,18 @@ public class ViewTransactionDto {
     @Schema(description = "Data e hora em que a transação foi criada", example = "2024-02-04T12:30:45")
     private LocalDateTime createdAt;
 
-    @Schema(description = "Data e hora de finalização da transação, se aplicável", example = "2024-02-05T14:20:10")
-    private LocalDateTime finishedAt;
-
-    @Schema(description = "Data e hora do cancelamento da transação, se aplicável", example = "null")
-    private LocalDateTime canceledAt;
-
     @Schema(description = "Valor da transação", example = "250.75")
     private BigDecimal amount;
 
     @Schema(description = "Tipo da transação", example = "INCOME")
     private TransactionType type;
 
-    @Schema(description = "Método de pagamento utilizado", example = "CREDIT_CARD")
-    private PaymentMethod paymentMethod;
-
     @Schema(description = "Descrição da transação", example = "Pagamento de Pedido")
     private String description;
 
-    @Schema(description = "ID do pedido associado à transação, se aplicável", example = "123")
-    private Long orderId;
+    @Schema(description = "Pedido associado à transação, se aplicável", example = "123")
+    private ViewOrderDto order;
 
-    @Schema(description = "ID da compra associada à transação, se aplicável", example = "456")
-    private Long purchaseId;
+    @Schema(description = "Compra associada à transação, se aplicável", example = "456")
+    private ViewPurchaseDto purchase;
 }
