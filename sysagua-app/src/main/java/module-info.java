90409fbd0167a5d3329a84fff500912e569b0ecf
Rafa <costtaraphael@gmail.com>
2025-02-09 17:32:57 -0300

    feat: novos exports e autorizações para jackson

--- sysagua-app/src/main/java/module-info.java
+++ sysagua-app/src/main/java/module-info.java
@@ -21,4 +21,9 @@ module edu.pies.sysaguaapp {
     opens edu.pies.sysaguaapp.controllers.fornecedor to javafx.fxml;
     exports edu.pies.sysaguaapp.controllers.produto;
     opens edu.pies.sysaguaapp.controllers.produto to javafx.fxml;
+    exports edu.pies.sysaguaapp.models.compras;
+    exports edu.pies.sysaguaapp.controllers.compras;
+    opens edu.pies.sysaguaapp.controllers.compras to javafx.fxml;
+    opens edu.pies.sysaguaapp.dtos.compra to com.fasterxml.jackson.databind;
+
 }
