0d223a5bdf462a55ed91c6bbc92ba1fdf285b287
CristianoMends <mendescristiano012@gmail.com>
2025-02-08 22:19:07 -0300

    refactor: adicionado busca por status de pagamento

--- sysagua-api/src/main/java/com/api/sysagua/repository/OrderRepository.java
+++ sysagua-api/src/main/java/com/api/sysagua/repository/OrderRepository.java
@@ -2,6 +2,7 @@ package com.api.sysagua.repository;
 
 import com.api.sysagua.enumeration.DeliveryStatus;
 import com.api.sysagua.enumeration.PaymentMethod;
+import com.api.sysagua.enumeration.PaymentStatus;
 import com.api.sysagua.model.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -23,6 +24,7 @@ public interface OrderRepository extends JpaRepository<Order, Long> {
             and ((:receivedAmountStart is null or :receivedAmountEnd is null) or o.receivedAmount between :receivedAmountStart and :receivedAmountEnd) 
             and ((:totalAmountStart is null or :totalAmountEnd is null) or o.totalAmount between :totalAmountStart and :totalAmountEnd) 
             and (:paymentMethod is null or o.paymentMethod = :paymentMethod) 
+            and (:paymentStatus is null or o.paymentStatus = :paymentStatus)
             and ((CAST(:createdAtStart as TIMESTAMP) is null or CAST(:createdAtEnd as TIMESTAMP) is null) or o.createdAt between :createdAtStart and :createdAtEnd) 
             and ((CAST(:finishedAtStart as TIMESTAMP) is null or CAST(:finishedAtEnd as TIMESTAMP) is null) or o.finishedAt between :finishedAtStart and :finishedAtEnd)
             order by o.id
@@ -41,7 +43,8 @@ public interface OrderRepository extends JpaRepository<Order, Long> {
             @Param("createdAtStart") LocalDateTime createdAtStart,
             @Param("createdAtEnd") LocalDateTime createdAtEnd,
             @Param("finishedAtStart") LocalDateTime finishedAtStart,
-            @Param("finishedAtEnd") LocalDateTime finishedAtEnd);
-
+            @Param("finishedAtEnd") LocalDateTime finishedAtEnd,
+            @Param("paymentStatus") PaymentStatus paymentStatus
+            );
 
 }
