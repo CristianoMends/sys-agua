54569b91459e69e298d8fdcf953b54be16a89ed9
josiasdev <francisco.batista67@alu.ufc.br>
2025-02-12 14:28:23 -0300

    remove: removi uma dependência que não precisava

--- sysagua-app/src/main/java/module-info.java
+++ sysagua-app/src/main/java/module-info.java
@@ -9,6 +9,7 @@ module edu.pies.sysaguaapp {
     requires java.xml.crypto;
     requires com.fasterxml.jackson.databind;
     requires static lombok;
+    requires java.desktop;
 
     opens edu.pies.sysaguaapp to javafx.fxml;
     exports edu.pies.sysaguaapp;
@@ -17,4 +18,7 @@ module edu.pies.sysaguaapp {
     exports edu.pies.sysaguaapp.models;
     exports edu.pies.sysaguaapp.controllers.estoque;
     opens edu.pies.sysaguaapp.controllers.estoque to javafx.fxml;
+    exports edu.pies.sysaguaapp.models.Pedido;
+    exports edu.pies.sysaguaapp.controllers.Clientes;
+    opens edu.pies.sysaguaapp.controllers.Clientes to javafx.fxml;
 }
