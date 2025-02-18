4d0319880e1af32e65c6577da4b2ba4db359ec08
josiasdev <francisco.batista67@alu.ufc.br>
2025-02-18 19:04:22 -0300

    feat: Criação do Usuario service

--- sysagua-app/src/main/java/edu/pies/sysaguaapp/services/UsuarioService.java
+++ sysagua-app/src/main/java/edu/pies/sysaguaapp/services/UsuarioService.java
@@ -1,4 +1,19 @@
 package edu.pies.sysaguaapp.services;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+
+import java.net.http.HttpClient;
+
 public class UsuarioService {
+
+    private static final String BASE_URL = "http://localhost:8080/orders";
+    private static HttpClient httpClient;
+    private static ObjectMapper objectMapper;
+
+    public  UsuarioService(){
+        this.httpClient = HttpClient.newHttpClient();
+        this.objectMapper = new ObjectMapper();
+        this.objectMapper.registerModule(new JavaTimeModule());
+    }
 }
