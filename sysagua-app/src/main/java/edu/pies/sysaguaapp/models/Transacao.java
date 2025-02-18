52605500fc8b651b160c41b332403988ec56e0d3
josiasdev <francisco.batista67@alu.ufc.br>
2025-02-18 19:33:34 -0300

    feat: Modelo de Transação

--- sysagua-app/src/main/java/edu/pies/sysaguaapp/models/Transacao.java
+++ sysagua-app/src/main/java/edu/pies/sysaguaapp/models/Transacao.java
@@ -1,4 +1,23 @@
 package edu.pies.sysaguaapp.models;
 
+
+import edu.pies.sysaguaapp.enumeration.Transacao.TransacaoTipo;
+import edu.pies.sysaguaapp.models.Pedido.Pedido;
+import edu.pies.sysaguaapp.models.compras.Compra;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
 public class Transacao {
+    private Long id;
+    private LocalDateTime createdAt;
+    private BigDecimal amount;
+    private TransacaoTipo type;
+    private String description;
+    private Pedido order;
+    private Compra purchase;
 }
