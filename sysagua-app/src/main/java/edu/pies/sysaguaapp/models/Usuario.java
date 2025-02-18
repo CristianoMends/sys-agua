1de4333ac663a219a507f5ea88f49530cf2977b3
josiasdev <francisco.batista67@alu.ufc.br>
2025-02-18 19:03:57 -0300

    feat: Criação do modelo de Usuário

--- sysagua-app/src/main/java/edu/pies/sysaguaapp/models/Usuario.java
+++ sysagua-app/src/main/java/edu/pies/sysaguaapp/models/Usuario.java
@@ -1,4 +1,18 @@
 package edu.pies.sysaguaapp.models;
 
+import edu.pies.sysaguaapp.enumeration.Usuarios.UserAccess;
+import edu.pies.sysaguaapp.enumeration.Usuarios.UserStatus;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class Usuario {
+    private Long id;
+    private String name;
+    private String phone;
+    private String email;
+    private String password;
+    private UserStatus status;
+    private UserAccess access;
 }
