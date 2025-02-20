1f07f31e6418d3966432df3a692648c0ff03a1a4
josiasdev <francisco.batista67@alu.ufc.br>
2025-02-19 23:56:56 -0300

    feat: Responsável por listar os usuários, criar usuários e inativar usuário

--- sysagua-app/src/main/java/edu/pies/sysaguaapp/controllers/Usuarios/UsuarioController.java
+++ sysagua-app/src/main/java/edu/pies/sysaguaapp/controllers/Usuarios/UsuarioController.java
@@ -1,7 +1,8 @@
 package edu.pies.sysaguaapp.controllers.Usuarios;
 
+import edu.pies.sysaguaapp.enumeration.Usuarios.UserAccess;
+import edu.pies.sysaguaapp.enumeration.Usuarios.UserStatus;
 import edu.pies.sysaguaapp.models.Usuario;
-import edu.pies.sysaguaapp.services.FornecedorService;
 import edu.pies.sysaguaapp.services.TokenManager;
 import edu.pies.sysaguaapp.services.UsuarioService;
 import javafx.animation.PauseTransition;
@@ -12,7 +13,6 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Cursor;
 import javafx.scene.Parent;
 import javafx.scene.control.*;
-import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseButton;
 import javafx.scene.layout.*;
 import javafx.util.Duration;
@@ -27,7 +27,7 @@ public class UsuarioController {
     private StackPane rootPane;
 
     @FXML
-    private TableView<Usuario> tabelaFornecedor;
+    private TableView<Usuario> tabelaUsuario;
 
     @FXML
     private HBox paginationContainer;
@@ -55,81 +55,58 @@ public class UsuarioController {
     @FXML
     public void initialize() {
         configurarTabela();
-        carregarFornecedores();
+        carregarUsuario();
         showMenuContext();
         btnAdicionar.setCursor(Cursor.HAND);
 
-        exibirInativosCheckBox.selectedProperty().addListener((observable, oldValue, newValue) -> carregarFornecedores());
+        exibirInativosCheckBox.selectedProperty().addListener((observable, oldValue, newValue) -> carregarUsuario());
     }
 
 
 
-    /*------------------- fornecedor --------------*/
+    /*------------------- usuario --------------*/
 
     @FXML
-    private void handleAddFornecedor() {
+    private void handleAddUsuario() {
         try {
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("/views/Fornecedor/AddFornecedor.fxml"));
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/views/Usuario/AddUsuario.fxml"));
             Parent form = loader.load();
 
             rootPane.getChildren().clear();
             rootPane.getChildren().add(form);
         } catch (Exception e) {
             e.printStackTrace();
-            System.out.println("Erro ao carregar formulário de fornecedor: " + e.getMessage());
+            System.out.println("Erro ao carregar formulário de usuario: " + e.getMessage());
         }
     }
 
     private void showMenuContext(){
         ContextMenu contextMenu = new ContextMenu();
 
-        MenuItem editarItem = new MenuItem("Editar fornecedor");
-        MenuItem inativarItem = new MenuItem("Inativar fornecedor");
+        MenuItem inativarUsuario = new MenuItem("Inativar Usuário");
 
-        contextMenu.getItems().addAll(editarItem, inativarItem);
-        editarItem.setOnAction(event -> handleEditarFornecedor());
-        inativarItem.setOnAction(event -> handleInativarFornecedor());
+        contextMenu.getItems().addAll(inativarUsuario);
 
-        tabelaFornecedor.setOnMouseClicked(event -> {
-            if (event.getButton() == MouseButton.SECONDARY && !tabelaFornecedor.getSelectionModel().isEmpty()) {
-                contextMenu.show(tabelaFornecedor, event.getScreenX(), event.getScreenY());
+        inativarUsuario.setOnAction(event -> handleInativarUsuario());
+
+        tabelaUsuario.setOnMouseClicked(event -> {
+            if (event.getButton() == MouseButton.SECONDARY && !tabelaUsuario.getSelectionModel().isEmpty()) {
+                contextMenu.show(tabelaUsuario, event.getScreenX(), event.getScreenY());
             } else {
                 contextMenu.hide();
             }
         });
     }
 
-    private void handleEditarFornecedor() {
-        Fornecedor fornecedorSelecionado = tabelaFornecedor.getSelectionModel().getSelectedItem();
-        if (fornecedorSelecionado != null) {
+    private void handleInativarUsuario() {
+        Usuario usuarioSelecionado = tabelaUsuario.getSelectionModel().getSelectedItem();
+        if (usuarioSelecionado != null) {
             try {
-                FXMLLoader loader = new FXMLLoader(getClass().getResource("/views/Fornecedor/AddFornecedor.fxml"));
-                Parent form = loader.load();
-
-                // Obter o controlador da nova tela
-                AddFornecedorController controller = loader.getController();
-                // Passar o ID do fornecedor para o controlador
-                controller.setFornecedorEditando(String.valueOf(fornecedorSelecionado.getId()));
-
-                rootPane.getChildren().clear();
-                rootPane.getChildren().add(form);
+                usuarioService.inativarUsuario(usuarioSelecionado, token);
+                tabelaUsuario.refresh();
             } catch (Exception e) {
                 e.printStackTrace();
-                System.out.println("Erro ao carregar formulário de fornecedor: " + e.getMessage());
-            }
-        }
-    }
-
-    private void handleInativarFornecedor() {
-        Fornecedor fornecedorSelecionado = tabelaFornecedor.getSelectionModel().getSelectedItem();
-        if (fornecedorSelecionado != null) {
-            try {
-                fornecedorService.inativarFornecedor(fornecedorSelecionado, token);
-                tabelaFornecedor.refresh();
-
-            } catch (Exception e) {
-                e.printStackTrace();
-                System.out.println("Erro ao inativar fornecedor: " + e.getMessage());
+                System.out.println("Erro ao inativar Usuario: " + e.getMessage());
             }
         }
     }
@@ -139,53 +116,49 @@ public class UsuarioController {
 
     private void configurarTabela() {
         // Configuração das colunas da tabela
-        TableColumn<Fornecedor, String> colunaRazaosocial = new TableColumn<>("Razão social");
-        colunaRazaosocial.setCellValueFactory(new PropertyValueFactory<>("socialReason"));
+        TableColumn<Usuario, String> colunaNome = new TableColumn<>("Nome");
+        colunaNome.setCellValueFactory(celldata -> {
+            String nome = celldata.getValue().getName();
+            String subname = celldata.getValue().getSurname();
+            return new SimpleStringProperty(nome + " " + subname);
+        });
+        colunaNome.setStyle("-fx-alignment: CENTER;");
 
-        TableColumn<Fornecedor, Long> colunaCodigo = new TableColumn<>("Código");
-        colunaCodigo.setCellValueFactory(cellData -> new SimpleObjectProperty<>(cellData.getValue().getId()));
-        colunaCodigo.setStyle("-fx-alignment: CENTER;");
-        colunaCodigo.setSortType(TableColumn.SortType.ASCENDING);
 
-        TableColumn<Fornecedor, String> colunaTelefone = new TableColumn<>("Telefone");
-        colunaTelefone.setCellValueFactory(new PropertyValueFactory<>("phone"));
+        TableColumn<Usuario, String> colunaEmail = new TableColumn<>("Email");
+        colunaEmail.setCellValueFactory(cellData -> new SimpleObjectProperty<>(cellData.getValue().getEmail()));
+        colunaEmail.setStyle("-fx-alignment: CENTER;");
+
+        TableColumn<Usuario, String> colunaCargo = new TableColumn<>("Cargo");
+        colunaCargo.setCellValueFactory(param -> {
+            UserAccess access = param.getValue().getAccess();
+
+            if (access != null) {
+                // Verifica se é um dos três valores permitidos
+                if (access == UserAccess.OWNER || access == UserAccess.EMPLOYEE || access == UserAccess.MANAGER) {
+                    return new SimpleObjectProperty<>(access.getDescription());
+                }
+            }
+            return new SimpleObjectProperty<>("");
+        });
+        colunaCargo.setStyle("-fx-alignment: CENTER;");
+
+        TableColumn<Usuario, String> colunaTelefone = new TableColumn<>("Telefone");
+        colunaTelefone.setCellValueFactory(cellData -> new SimpleObjectProperty<>(cellData.getValue().getPhone()));
         colunaTelefone.setStyle("-fx-alignment: CENTER;");
 
-        TableColumn<Fornecedor, String> colunaCnpj = new TableColumn<>("CNPJ");
-        colunaCnpj.setCellValueFactory(new PropertyValueFactory<>("cnpj"));
-        colunaCnpj.setStyle("-fx-alignment: CENTER;");
 
-        TableColumn<Fornecedor, String> colunaEndereco = new TableColumn<>("Endereço");
-        colunaEndereco.setCellValueFactory(celldata -> {
-            String rua = celldata.getValue().getAddress().getStreet();
-            String numero = celldata.getValue().getAddress().getNumber();
-            String bairro = celldata.getValue().getAddress().getNeighborhood();
-            return new SimpleStringProperty(rua + ", " + numero + ", " + bairro);
-        });
+        tabelaUsuario.getColumns().addAll(colunaNome, colunaEmail, colunaCargo, colunaTelefone );
 
-        TableColumn<Fornecedor, String> columnCidade = new TableColumn<>("Cidade");
-        columnCidade.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getAddress().getCity()));
+        tabelaUsuario.setItems(usuarioObservable);
 
-        TableColumn<Fornecedor, String> colunaStatus = new TableColumn<>("Status");
-        colunaStatus.setCellValueFactory(cellData -> {
-            if (cellData.getValue().getActive()) {
-                return new SimpleStringProperty("Ativo");
-            }
-            return new SimpleStringProperty("Inativo");
-        });
-        colunaStatus.setStyle("-fx-alignment: CENTER;");
-
-        tabelaFornecedor.getColumns().addAll(colunaCodigo,colunaRazaosocial, colunaTelefone, colunaCnpj, colunaEndereco, columnCidade, colunaStatus);
-
-        tabelaFornecedor.setItems(fornecedorObservable);
-
-        tabelaFornecedor.setRowFactory(tv -> new TableRow<>() {
+        tabelaUsuario.setRowFactory(tv -> new TableRow<Usuario>() {
             @Override
-            protected void updateItem(Fornecedor fornecedor, boolean empty) {
-                super.updateItem(fornecedor, empty);
-                if (fornecedor == null || empty) {
+            protected void updateItem(Usuario usuario, boolean empty) {
+                super.updateItem(usuario, empty);
+                if (usuario == null || empty) {
                     setStyle("");
-                } else if (!fornecedor.isActive()) {
+                } else if (!usuario.getStatus().getDescription().equals(usuario.getAccess().getDescription())) {
                     setStyle("-fx-text-fill: red;");
                 } else {
                     setStyle("");
@@ -194,26 +167,25 @@ public class UsuarioController {
         });
     }
 
-    private void carregarFornecedores() {
+    private void carregarUsuario() {
         try {
             String token = TokenManager.getInstance().getToken();
-            List<Fornecedor> todosFornecedores = fornecedorService.buscarFornecedores(token);
+            List<Usuario> todosUsuario = usuarioService.buscarUsuario(token);
 
             if (!exibirInativosCheckBox.isSelected()) {
-                todosFornecedores.removeIf(fornecedor -> !fornecedor.getActive());
+                todosUsuario.removeIf(usuario -> usuario.getStatus() != UserStatus.ACTIVE);
             }
 
             // Calcula o total de páginas
-            totalPaginas = (int) Math.ceil((double) todosFornecedores.size() / itensPorPagina);
+            totalPaginas = (int) Math.ceil((double) todosUsuario.size() / itensPorPagina);
 
             // Filtra os fornecedores para a página atual
             int inicio = paginaAtual * itensPorPagina;
-            int fim = Math.min(inicio + itensPorPagina, todosFornecedores.size());
-            List<Fornecedor> fornecedoresPagina = todosFornecedores.subList(inicio, fim);
+            int fim = Math.min(inicio + itensPorPagina, todosUsuario.size());
+            List<Usuario> usuariosPagina = todosUsuario.subList(inicio, fim);
 
             // Atualiza a lista observável
-            fornecedorObservable.setAll(fornecedoresPagina);
-
+            usuarioObservable.setAll(usuariosPagina);
             atualizarEstadoBotoes();
             atualizarPaginacao();
 
@@ -244,14 +216,14 @@ public class UsuarioController {
 
     private void irParaPagina(int pagina) {
         paginaAtual = pagina;
-        carregarFornecedores();
+        carregarUsuario();
     }
 
     @FXML
     private void handlePaginaAnterior() {
         if (paginaAtual > 0) {
             paginaAtual--;
-            carregarFornecedores();
+            carregarUsuario();
         }
     }
 
@@ -259,7 +231,7 @@ public class UsuarioController {
     private void handleProximaPagina() {
         if (paginaAtual < totalPaginas - 1) {
             paginaAtual++;
-            carregarFornecedores();
+            carregarUsuario();
         }
     }
 
