b756a691b5c792c606195aa984c5a0d857cf7d26
CristianoMends <mendescristiano012@gmail.com>
2025-02-21 13:23:12 -0300

    refactor: ajustado migrations com as alterações

--- sysagua-api/src/main/resources/db/migration/h2/V9__create_table_product_purchase.sql
+++ sysagua-api/src/main/resources/db/migration/h2/V9__create_table_product_purchase.sql
@@ -2,7 +2,7 @@ CREATE TABLE product_purchases (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
     purchase_id BIGINT,
     product_id BIGINT,
-    purchase_price NUMERIC(10,2) NOT NULL,
+    unit_price NUMERIC(10,2) NOT NULL,
     quantity INT NOT NULL,
     total NUMERIC(10,2),
     FOREIGN KEY (purchase_id) REFERENCES purchases(id) ON DELETE CASCADE
