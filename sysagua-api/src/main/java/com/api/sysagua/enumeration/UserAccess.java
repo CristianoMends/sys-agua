dd66bdfca10ec02b063aedb404db3ebb44b7b4b4
CristianoMends <mendescristiano012@gmail.com>
2025-02-21 13:28:03 -0300

    refactor: adicionado password no update, e ajustado primeiro usuario

--- sysagua-api/src/main/java/com/api/sysagua/enumeration/UserAccess.java
+++ sysagua-api/src/main/java/com/api/sysagua/enumeration/UserAccess.java
@@ -6,7 +6,7 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum UserAccess {
-    DEVELOPER("Desenvolvedor"),
+
     OWNER("Dono"),
     EMPLOYEE("Funcionario"),
     MANAGER("Gestor");
