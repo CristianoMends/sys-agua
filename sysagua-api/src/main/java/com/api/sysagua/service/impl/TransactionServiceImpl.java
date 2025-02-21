c990c8788e1865a9853242e53913d5e6bf8e4c2e
CristianoMends <mendescristiano012@gmail.com>
2025-02-21 13:30:21 -0300

    refactor: ajustado para criar transações de acordo com a instancia

--- sysagua-api/src/main/java/com/api/sysagua/service/impl/TransactionServiceImpl.java
+++ sysagua-api/src/main/java/com/api/sysagua/service/impl/TransactionServiceImpl.java
@@ -2,7 +2,9 @@ package com.api.sysagua.service.impl;
 
 import com.api.sysagua.enumeration.PaymentMethod;
 import com.api.sysagua.enumeration.TransactionType;
+import com.api.sysagua.model.Order;
 import com.api.sysagua.model.Purchase;
+import com.api.sysagua.model.Transactable;
 import com.api.sysagua.model.Transaction;
 import com.api.sysagua.observer.TransactionObserver;
 import com.api.sysagua.repository.TransactionRepository;
@@ -22,8 +24,25 @@ public class TransactionServiceImpl implements TransactionService, TransactionOb
     private UserService userService;
 
     @Override
-    public void update(Purchase purchase, BigDecimal amount, PaymentMethod paymentMethod, String description) {
-        createTransaction(purchase, amount, paymentMethod, description);
+    public void update(Transactable transactable, BigDecimal amount, PaymentMethod paymentMethod, String description) {
+        if (transactable instanceof Order) {
+            createTransaction((Order) transactable, amount, paymentMethod, description);
+        } else {
+            createTransaction((Purchase) transactable, amount, paymentMethod, description);
+        }
+    }
+
+    private void createTransaction(Order order, BigDecimal amount, PaymentMethod paymentMethod, String description) {
+        var user = this.userService.getLoggedUser();
+        var t = new Transaction(
+                amount,
+                TransactionType.EXPENSE,
+                paymentMethod,
+                description,
+                user,
+                order
+        );
+        this.transactionRepository.save(t);
     }
 
     private void createTransaction(Purchase purchase, BigDecimal amount, PaymentMethod paymentMethod, String description) {
